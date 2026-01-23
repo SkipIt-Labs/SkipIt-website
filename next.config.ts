fc94cf2c37043cf733f131f4a35562818853369d
papura-octavian <kebulann@gmail.com>
2026-01-23 23:22:04 +0200

    updated for new domain

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
-const basePath = "/SkipIt-website";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: `${basePath}/`,
   trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
+
   images: {
     unoptimized: true,
   },
