849ab8f4466dd48ac60645e361e26f2af15a3aae
papura-octavian <kebulann@gmail.com>
2026-01-23 18:05:14 +0200

    adjust path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/SkipIt-website",
+  assetPrefix: "/SkipIt-website/",
   images: {
     unoptimized: true,
   },
