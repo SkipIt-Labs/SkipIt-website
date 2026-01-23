55a0201b5b6262fc04f5e7ea7af627cd4e3d94aa
papura-octavian <kebulann@gmail.com>
2026-01-23 18:39:50 +0200

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
-const basePath = "/SkipIt-website";
+const basePath = "/Skiplit-website";
 
 const nextConfig: NextConfig = {
   output: "export",
   basePath,
   assetPrefix: `${basePath}/`,
+  trailingSlash: true,
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
   },
